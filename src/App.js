68483bc875e05a8cfb10d48121c9ad76d1059bf2
BlueTR <BlueTR@Homes-iMac.local>
2025-04-24 14:52:56 -0400

    added cart modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart";
 
 // function App Component
 function App() {
@@ -18,6 +19,7 @@ function App() {
   // JSX
   return (
     <CartProvider>
+      <Cart />
       <Header />
       <main>
         <Meals />
